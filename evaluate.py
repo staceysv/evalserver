443a03952240380d2b67e45e967f8ad87e8942e2
Stacey Svetlichnaya <stacey@wandb.com>
2021-01-21 22:46:45 -0800

    all metrics logging

--- evaluate.py
+++ evaluate.py
@@ -11,7 +11,32 @@ from PIL import Image
 import util
 import wandb
 
+def per_category_metrics(ious, fps, fns):
+  metrics = {}
+  for metric, category in zip(["iou", "fps", "fns"], [ious, fps, fns]):
+    for category_name, ids in util.CITYSCAPE_IDS.items():
+      category_metric = np.mean([category[class_id] for class_id in ids])
+      metrics[metric + "_" + category_name] = category_metric
+  return metrics
+
+# ious is 50 rows of 19 columns
+def mean_metrics(ious, fps, fns):
+  # mean per class ious
+  ious_np = np.mean(ious, axis=0)
+  
+  fps_floats = np.array(fps)
+  norm_fps = fps_floats / util.TOTAL_PIXELS
+  # mean normalized per class fps
+  mean_norm_fps = np.mean(norm_fps, axis=0)
+  fns_floats = np.array(fns)
+  norm_fns = fns_floats / util.TOTAL_PIXELS
+  mean_norm_fns = np.mean(norm_fns, axis=0)
+
+  return ious_np, mean_norm_fps, mean_norm_fns
+  
+
 # 2D version
+# return false pos and false neg per class
 def iou_flat(mask_guess, mask_b, class_id):
     # 4x upsample mask_a
     mask_a = mask_guess.repeat(2, axis=0).repeat(2, axis=1)
@@ -22,6 +47,40 @@ def iou_flat(mask_guess, mask_b, class_id):
       return np.nan_to_num(0.0, 0.0, 0.0, 0.0)
     else:
       return np.nan_to_num(intersection / union, 0.0, 0.0, 0.0)
+
+def smooth(num, denom):
+  if np.isclose(denom, 0):
+    return np.nan_to_num(0.0, 0.0, 0.0, 0.0)
+  else:
+    return np.nan_to_num(num / denom, 0.0, 0.0, 0.0)
+
+# for every class_id
+def pixel_count(guess, truth):
+  iou_scores = {}
+  net_fp = 0
+  net_fn = 0
+  net_tp = 0
+  # NOTE: void class doesn't contribute
+  for class_id in range(19):
+    fp = ((guess == class_id) & (truth != class_id)).sum(axis=(0,1))
+    fn = ((guess != class_id) & (truth == class_id)).sum(axis=(0,1))
+    tp = ((guess == class_id) & (truth == class_id)).sum(axis=(0,1))
+    iou = smooth(float(tp), float(tp + fp + fn)) #, 0.0, 0.0, 0.0)
+    iou_scores[class_id] = {"fp": fp, "fn" : fn, "tp" : tp, "iou" : iou}
+    net_fp += fp
+    net_fn += fn
+    net_tp += tp
+  net_iou = smooth(float(net_tp), float(net_tp + net_fp + net_fn))
+  iou_scores["net_tp"] = net_tp
+  iou_scores["net_fp"] = net_fp
+  iou_scores["net_fn"] = net_fn
+  iou_scores["net_iou"] = net_iou
+  return iou_scores 
+
+def score_masks(mask_guess, mask_truth):
+  guess = mask_guess.repeat(2, axis=0).repeat(2, axis=1)
+  return pixel_count(guess, mask_truth)
+
 
 def iou_3D(mask_a, mask_b, class_id):
     return np.nan_to_num(((mask_a == class_id) & (mask_b == class_id)).sum(axis=(1,2)) / ((mask_a == class_id) | (mask_b == class_id)).sum(axis=(1,2)), 0, 0, 0)
@@ -51,17 +110,68 @@ truth = pd.DataFrame({'id' : true_ids, "truth" : [i for i in range(len(true_imag
 results = guess.join(truth, lsuffix="_guess", rsuffix="_truth")
 
 # now log a final table
-eval_table = wandb.Table(columns=["id", "prediction", "ground_truth", "iou"])
+columns=["id", "prediction", "ground_truth"]
+columns.extend(["overall IOU", "false positive", "false negative"])
+
+#eval_table = wandb.Table(columns=columns) #["id", "prediction", "ground_truth", "iou"])
 
 # OK now they are joined
+#for index, row in results.iterrows():
+  #s = iou_flat(guess_images[row["guess"]], true_images[row["truth"]], 0)
+#  scores = score_masks(guess_images[row["guess"]], true_images[row["truth"]])
+  # all the scores per class
+#  eval_table.add_data(row["id_truth"], guess_pretty[row["guess"]], true_pretty[row["truth"]], scores["net_iou"], scores["net_fp"], scores["net_fn"])
+#  wandb.log({"net_fp" : scores["net_fp"], "net_fn" : scores["net_fn"], "net_iou" : scores["net_iou"]})
+
+ious = []
+fps = []
+fns = []
+# make a full score table
+columns.extend(["iou_" + s for s in util.BDD_CLASSES[:-1]])
+columns.extend(["fn_" + s for s in util.BDD_CLASSES[:-1]])
+columns.extend(["fp_" + s for s in util.BDD_CLASSES[:-1]])
+full_num_table = wandb.Table(columns=columns)
 for index, row in results.iterrows():
-  s = iou_flat(guess_images[row["guess"]], true_images[row["truth"]], 0)
-  eval_table.add_data(row["id_truth"], guess_pretty[row["guess"]], true_pretty[row["truth"]], s)
+  #s = iou_flat(guess_images[row["guess"]], true_images[row["truth"]], 0)
+  scores = score_masks(guess_images[row["guess"]], true_images[row["truth"]])
+  # all the scores per class
+  r = [row["id_truth"], guess_pretty[row["guess"]], true_pretty[row["truth"]], scores["net_iou"], scores["net_fp"], scores["net_fn"]]
+  class_ious = [scores[class_id]["iou"] for class_id in range(19)]
+  ious.append(class_ious)
+  r.extend(class_ious)
+  class_fns = [scores[class_id]["fn"] for class_id in range(19)]
+  fns.append(class_fns)
+  r.extend(class_fns)
+  class_fps = [scores[class_id]["fp"] for class_id in range(19)]
+  fps.append(class_fps)
+  r.extend(class_fps)
+  
+  full_num_table.add_data(*r)
+  #wandb.log({"net_fp" : scores["net_fp"], "net_fn" : scores["net_fn"], "net_iou" : scores["net_iou"]})
+
+# what if we make this a dataframe
+# now what.
+# - report mean across all ious
+# - report mean across all fps and fns (TODO: normalize?)
+# - report per-category mean iou
+mean_ious, mean_fps, mean_fns = mean_metrics(ious, fps, fns)
+iou_d = { "iou_" + util.BDD_CLASSES[i] : m  for i, m in enumerate(mean_ious)}
+fps_d = { "fps_" + util.BDD_CLASSES[i] : m  for i, m in enumerate(mean_fps)}
+fns_d = { "fns_" + util.BDD_CLASSES[i] : m  for i, m in enumerate(mean_fns)}
+wandb.log(iou_d)
+wandb.log(fps_d)
+wandb.log(fns_d)
+# TODO convert to dict
+# wandb.log
+# then we'll do per-category later
+per_category = per_category_metrics(mean_ious, mean_fps, mean_fns)
+print(per_category)
+wandb.log(per_category)
 
 results_at = wandb.Artifact("eval_results", type="results")
-results_at.add(eval_table, "eval_results")
+#results_at.add(eval_table, "eval_results")
+results_at.add(full_num_table, "full_num_results")
 run.log_artifact(results_at)
-
 
 # join guess_images and true_images by id
 # score each pair
--- util.py
+++ util.py
@@ -8,6 +8,8 @@ ANSWER_PROJECT = "answers_evalserve"
 SUBMIT_PROJECT = "evalserve_predict"
 NUM_EXAMPLES = 50
 
+# total number of pixels in each training image
+TOTAL_PIXELS = float(720*1280)
 
 # classes
 BDD_CLASSES = [
@@ -18,6 +20,28 @@ BDD_CLASSES = [
 BDD_IDS = list(range(len(BDD_CLASSES) - 1)) + [255]
 class_set = wandb.Classes([{'name': name, 'id': id}
                            for name, id in zip(BDD_CLASSES, BDD_IDS)])
+
+CS_CATEGORIES = {
+  "flat" : ["road", "sidewalk"],
+  "human" : ["person", "rider"],
+  "vehicle" : ["car", "bus", "truck", "motorcycle", "bicycle", "train"],
+  "construction" : ["building", "wall", "fence"],
+  "object" : ["pole", "traffic light", "traffic sign"],
+  "nature" : ["vegetation", "terrain"],
+  "sky" : ["sky"]
+}
+
+CITYSCAPE_IDS = {
+  "flat" : [0, 1],
+  "human" : [11, 12], 
+  "vehicle" : [13, 14, 15, 16, 17, 18],
+  "construction" : [2, 3, 4], 
+  "object" : [5, 6, 7],
+  "nature" : [8, 9],
+  "sky" : [10] 
+}
+
+
 
 # wrapper for logging masks to W&B
 def wb_mask(bg_img, pred_mask=[], true_mask=[]):
